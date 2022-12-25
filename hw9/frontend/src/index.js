10129e8a01cdb768324ea7299d738e3485283719
Hon Chi Kit <kinoras@kinoras.com>
2022-12-25 18:03:49 +0800

    Update index.js

--- hw9/frontend/src/index.js
+++ hw9/frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./Containers/App.js";
-import reportWebVitals from "./reportWebVitals.js";
-import { ScoreCardProvider } from "./hooks/useScoreCard.js";
+import App from "./Containers/App";
+import reportWebVitals from "./reportWebVitals";
+import { ScoreCardProvider } from "./hooks/useScoreCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,5 +17,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
 reportWebVitals();
